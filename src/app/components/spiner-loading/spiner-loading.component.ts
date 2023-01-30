2cf9f345efa8a92d6c324a3360370fa11e2bfef0
mohammadkamraan <kamyzenon@gmail.com>
2023-01-30 04:31:15 -0800

    spinner loading component added

--- src/app/components/spiner-loading/spiner-loading.component.ts
+++ src/app/components/spiner-loading/spiner-loading.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-spiner-loading',
   templateUrl: './spiner-loading.component.html',
-  styleUrls: ['./spiner-loading.component.sass']
+  styleUrls: ['./spiner-loading.component.scss'],
 })
 export class SpinerLoadingComponent {
-
+  @Input() size: 'small' | 'medium' | 'large';
+  @Input() color: 'white' | 'dark' | 'gray';
 }
